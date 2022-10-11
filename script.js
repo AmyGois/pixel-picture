c5999392cd7113fea53a64822e0ef48e94d3b6ea
AmyGois <ahagois@hotmail.com>
2022-10-11 20:47:36 +0100

    Create random colours option

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const container = document.querySelector('.container');
 
 const input = document.querySelector('.inputBtn');
 
-const eraserBtn = document.querySelector('#eraserBtn');
 const blackBtn = document.querySelector('#blackBtn');
 const randomBtn = document.querySelector('#randomBtn');
+const greysBtn = document.querySelector('#greysBtn');
 
-/* F1 - make X squares appear within a lager square - Requires F2 */
+const eraserBtn = document.querySelector('#eraserBtn');
+const clearBtn = document.querySelector('#clearBtn');
+
+/* F1 - make X squares appear within a lager square - Requires F2.1 */
 
 function makeSquares(length) {
     for(i = 1; i <= length; i++) {
@@ -27,56 +30,93 @@ makeSquares(16);
 
 //
 
-/* F2 - make squares black when hovered over with mouse */
+/* F2 - Different colour options for grid */
 
+// F2.1 - Black
 function colorSquareBlack(e) {
     e.target.style.backgroundColor = '#000';
 }
 
-//
-
-/* F3 - make squares white when hovered over with mouse */
-
+//F2.2 - White
 function colorSquareWhite(e) {
     e.target.style.backgroundColor = '#fff';
 }
 
+//F2.3 - Random colours
+function colorSquareRandom(e) {
+    const randomColor = Math.floor(Math.random() * 10);
+
+    switch(randomColor) {
+        case 0:
+            e.target.style.backgroundColor = '#4f6b48';
+            break;
+        case 1:
+            e.target.style.backgroundColor = '#FA7070';
+            break;
+        case 2:
+            e.target.style.backgroundColor = '#C6EBC5';
+            break;
+        case 3:
+            e.target.style.backgroundColor = '#fbf2cf';
+            break;
+        case 4:
+            e.target.style.backgroundColor = '#faea70';
+            break;
+        case 5:
+            e.target.style.backgroundColor = '#70faf3';
+            break;
+        case 6:
+            e.target.style.backgroundColor = '#663a58';
+            break;
+        case 7:
+            e.target.style.backgroundColor = '#502f33';
+            break;
+        case 8:
+            e.target.style.backgroundColor = '#e7c5eb';
+            break;
+        case 9:
+            e.target.style.backgroundColor = '#ac70fa';
+            break;
+    }
+}
+
 //
 
-/* F - Change what colour the sqares get filled in with, and clear other event listeners */
+/* F - Change what colour the squares get filled in with, and clear other event listeners, when button is clicked  - Requires F2*/
 
 function changeColors(colorChoice) {
     let squares = document.querySelectorAll('.square');
     squares.forEach((square) => {
         square.removeEventListener('mouseenter', colorSquareBlack);
         square.removeEventListener('mouseenter', colorSquareWhite);
+        square.removeEventListener('mouseenter', colorSquareRandom);
     });
     
     if(colorChoice === 'black') {
         squares.forEach((square) =>  square.addEventListener('mouseenter', colorSquareBlack));
     } else if(colorChoice === 'white') {
         squares.forEach((square) =>  square.addEventListener('mouseenter', colorSquareWhite));
+    } else if(colorChoice === 'random') {
+        squares.forEach((square) =>  square.addEventListener('mouseenter', colorSquareRandom));
     }
     
 }
 
 blackBtn.addEventListener('click', () => changeColors('black'));
+randomBtn.addEventListener('click', () => changeColors('random'));
 eraserBtn.addEventListener('click', () => changeColors('white'));
 
+//
 
-/* F - remove all the squares in the container div */
+/* F - Make a new grid of squares according to the number chosen in the input field, and clear previous grid*/
+
+input.addEventListener('click', changeSquares);
 
 function removeSquares() {
     let lines = document.querySelectorAll('.line');
 
     lines.forEach((line) => line.remove());
 }
-
-//
-
-/* F - Make a new grid of squares according to the number chosen in the input field - Requires F*/
-
-input.addEventListener('click', changeSquares);
 
 function changeSquares() {
     let numberOfSquares = document.querySelector('.inputField').value;
